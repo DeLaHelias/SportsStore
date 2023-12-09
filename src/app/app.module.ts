a1020676acd5c9262b6b2843d71abd88539efdd8
Skip Keats <skip.keats@gmail.com>
2023-12-09 13:39:40 -0500

    Initial store build.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from 'src/store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
