9135cd2876d06557b8b21690ffdb9d9abf89e13f
Skip Keats <skip.keats@gmail.com>
2023-12-30 20:02:11 -0500

    Admin section laid out more completely. Material not working correctly.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,25 +1,56 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { AuthComponent } from "./auth.component";
-import { AdminComponent } from "./admin.component";
-import { AuthGuard } from "./auth.guard";
-import { MaterialFeatures } from "./material.module";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AdminComponent } from './admin.component';
+import { AuthComponent } from './auth.component';
+import { AuthGuard } from './auth.guard';
+import { MaterialFeatures } from './material.module';
+import { OrderTableComponent } from './orderTable.component';
+import { ProductEditorComponent } from './productEditor.component';
+import { ProductTableComponent } from './productTable.component';
 
 let routing = RouterModule.forChild([
     {
         path: "auth",
         component: AuthComponent,
     },
-    {
-        path: "main",
-        component: AdminComponent,
-    },
+    // {
+    //     path: "main",
+    //     component: AdminComponent,
+    // },
+    // {
+    //     path: "main",
+    //     component: AdminComponent,
+    //     canActivate: [AuthGuard],
+    // },
     {
         path: "main",
         component: AdminComponent,
         canActivate: [AuthGuard],
+        children: [
+            {
+                path: "products/:mode/:id",
+                component: ProductEditorComponent,
+            },
+            {
+                path: "products/:mode",
+                component: ProductEditorComponent,
+            },
+            {
+                path: "products",
+                component: ProductTableComponent,
+            },
+            {
+                path: "orders",
+                component: OrderTableComponent,
+            },
+            {
+                path: "**",
+                redirectTo: "products",
+            },
+        ],
     },
     {
         path: "**", 
@@ -37,6 +68,9 @@ let routing = RouterModule.forChild([
     declarations: [
         AuthComponent,
         AdminComponent,
+        OrderTableComponent,
+        ProductEditorComponent,
+        ProductTableComponent,
     ],
     providers: [ AuthGuard, ]
 })
--- src/app/admin/material.module.ts
+++ src/app/admin/material.module.ts
@@ -1,6 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const features: any[] = [];
+const features: any[] = [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+];
 
 @NgModule({
     imports: [features],
