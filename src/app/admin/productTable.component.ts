4caa1fbe8dfd55dabb31f2ad37390876eb1dc738
Skip Keats <skip.keats@gmail.com>
2024-01-01 12:08:09 -0500

    Added Material paginator to the product table.

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,4 +1,5 @@
-import { Component, IterableDiffer, IterableDiffers } from '@angular/core';
+import { Component, IterableDiffer, IterableDiffers, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { Product } from '../model/product.model';
@@ -18,6 +19,9 @@ export class ProductTableComponent {
     dataSource = new MatTableDataSource<Product>(this.repository.getProducts());
     differ: IterableDiffer<Product>;
 
+    @ViewChild(MatPaginator)
+    paginator?: MatPaginator;
+
     constructor(
         private repository: ProductRepository,
         differs: IterableDiffers,
@@ -29,6 +33,12 @@ export class ProductTableComponent {
         let changes = this.differ?.diff(this.repository.getProducts());
         if (changes != null) {
             this.dataSource.data = this.repository.getProducts();
+        }
+    }
+
+    ngAfterViewInit() {
+        if (this.paginator) {
+            this.dataSource.paginator = this.paginator;
         }
     }
 
